398ae42d46c61b441eb54db42e3c19416d5ae563
Ganzorig Erdenebat <the.ganzorig25@gmail.com>
2018-04-25 11:41:55 -0500

    Bug fix

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,8 +2,13 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
+import { AuthService } from './../services/auth.service';
+import { Router } from '@angular/router';
+
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
@@ -11,6 +16,7 @@ export class AuthGuard implements CanActivate {
     if(token) {
       return true;
     } else {
+      this.router.navigate(['/login']);
       return false;
     }
   }
--- src/app/guards/nauth.guard.ts
+++ src/app/guards/nauth.guard.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
+import { AuthService } from './../services/auth.service';
+import { Router } from '@angular/router';
+
 @Injectable()
 export class NAuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     const token = localStorage.getItem('token');
     if(token) {
+      this.router.navigate(['/']);
       return false;
     } else {
       return true;
